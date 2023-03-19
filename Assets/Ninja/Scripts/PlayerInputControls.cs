b6e1999d92423f57a0a7038f47c09faf7eab9299
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-19 19:14:26 +0530

    added jump, added sprint

--- Assets/Ninja/Scripts/PlayerInputControls.cs
+++ Assets/Ninja/Scripts/PlayerInputControls.cs
@@ -7,15 +7,35 @@ namespace Ninja
 	public class PlayerInputControls : MonoBehaviour
 	{
 		[HideInInspector] public Vector2 move;
+		[HideInInspector] public bool jump;
+		[HideInInspector] public bool sprint;
 
 		public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
 
 		public void MoveInput(Vector2 newMoveInputDirection)
 		{
 			move = newMoveInputDirection;
+		}
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
 		}
 	}
 
--- Assets/Ninja/Scripts/PlayerMovement.cs
+++ Assets/Ninja/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Ninja
 {
@@ -8,29 +9,89 @@ namespace Ninja
 	{
 
 		private PlayerInputControls _playerInputControls;
+		private PlayerInput _input;
 		private CharacterController _characterController;
 
-		[SerializeField] private float moveSpeed;
+		private InputAction jumpAction;
+		private InputAction sprintAction;
+		
+		private float currentSpeed;
 
+		[SerializeField] private float moveSpeed;
+		[SerializeField] private float sprintSpeed;
+		[SerializeField] private float gravity;
+		[SerializeField] private float jumpHeight;
+
+		private bool isGrounded;
 		private float horizontalInput, verticalInput;
 		private Vector3 playerDirection;
+		private Vector3 gravityVector;
 
-		private void Awake()
+		private void Start()
 		{
 			_playerInputControls = GetComponent<PlayerInputControls>();
 			_characterController = GetComponent<CharacterController>();
+
+			_input = GetComponent<PlayerInput>();
+
+			jumpAction = _input.actions["Jump"];
+			sprintAction = _input.actions["Sprint"];
+
+			if(gravity > 0)
+			{
+				gravity *= -1f;
+			}
 		}
 
 		private void Update()
 		{
-			Vector2 moveDirection = _playerInputControls.move;
-			horizontalInput = _playerInputControls.move.x;
-			verticalInput = _playerInputControls.move.y;
-
-			playerDirection = transform.forward * verticalInput + transform.right * horizontalInput;
-			_characterController.Move(playerDirection * moveSpeed * Time.deltaTime);
+			groundCheck();
+			PlayerMove();
+			PlayerSprint();
+			Jump();
 			
 		}
 
+		public void groundCheck()
+		{
+			isGrounded = _characterController.isGrounded;
+			if(isGrounded && gravityVector.y < 0)
+			{
+				gravityVector.y = 0;
+			}
+		}
+
+		public void PlayerMove()
+		{
+			Vector2 moveDirection = _playerInputControls.move;
+			horizontalInput = moveDirection.x;
+			verticalInput = moveDirection.y;
+
+			playerDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+
+			_characterController.Move(playerDirection * currentSpeed * Time.deltaTime);
+		}
+
+		public void PlayerSprint()
+		{
+			if(sprintAction.WasPressedThisFrame())
+			{
+				currentSpeed  = sprintSpeed;
+			}
+			else if(sprintAction.WasReleasedThisFrame())
+			{
+				currentSpeed = moveSpeed;
+			}
+		}
+
+		public void Jump()
+		{
+			if(jumpAction.WasPressedThisFrame() && isGrounded)
+			{
+				gravityVector.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
+			}
+			gravityVector.y += gravity * Time.deltaTime;
+			_characterController.Move(gravityVector * Time.deltaTime);
+		}
 	}
 }
